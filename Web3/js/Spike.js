c98cfef46c8891b2cb7761f1adf3cd943b7488aa
threeangel <125662739+threeangel@users.noreply.github.com>
2025-12-02 15:09:30 +0300

    Delete Web3 directory

--- Web3/js/Spike.js
+++
@@ -1,9 +0,0 @@
-// js/Spike.js
-export default class Spike {
-  constructor(x, y, width = 20, height = 20) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
-}
--- Web3/js/main.js
+++
@@ -1,9 +0,0 @@
-import Game from './Game.js';
-
-window.addEventListener('load', () => {
-  const canvas = document.getElementById('gameCanvas');
-  const ctx = canvas.getContext('2d');
-
-  const game = new Game(canvas, ctx);
-  game.start();
-});
